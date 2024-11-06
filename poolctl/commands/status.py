dc4382dce7eae4bd1e754f2633c7eddbb754307d
sebi364 <110155948+sebi364@users.noreply.github.com>
2024-11-06 12:11:27 +0100

    added detailed pool status

--- poolctl/commands/status.py
+++ poolctl/commands/status.py
@@ -3,6 +3,14 @@ from support.log import log, console
 from support.pveapi import get, post
 from rich.table import Table, box
 
+def paint_status(status):
+    if status == 'running':
+        return f"[bold green]{status}[/]"    
+    elif status == 'stopped':
+        return f"[bold red]{status}[/]"
+    else:
+        return f"[bold yellow]{status}[/]"
+
 def pool_overview():
     output = []
 
@@ -60,7 +68,48 @@ def pool_overview():
     console.print(table)
 
 def pool_detail(pool):
-    console.print(get(f'pools/{pool}').json())
+    output = []
+    for m in get(f'pools/{pool}').json()['data']['members']:
+        output.append([
+            m['status'],
+            m['name'],
+            m['vmid'],
+            m['type'],
+            m['maxmem'],
+            m['mem'],
+            m['maxcpu'],
+            m['cpu'],
+            m['maxdisk'],
+            bool(m['template']),
+        ])
+    
+    table = Table(box=box.MINIMAL, highlight=True, show_footer=True)
+    table.add_column("Status:")
+    table.add_column("Name:")
+    table.add_column("Vmid:")
+    table.add_column("Type:")
+    table.add_column("Memory (aloc.):", footer=f"{round(sum([i[4] for i in output]) / 2**30, 2)} GB")
+    table.add_column("Memory (used):", footer=f"{round(sum([i[5] for i in output]) / 2**30, 2)} GB")
+    table.add_column("Core (aloc.):", footer=f"{round(sum([i[6] for i in output]), 2)} CPU")
+    table.add_column("Core (used):", footer=f"{round(sum([i[7] for i in output]), 2)} CPU")
+    table.add_column("Storage:", footer=f"{round(sum([i[8] for i in output]) / 2**30, 2)} GB")
+    table.add_column("Template:")
+
+    for r in output:
+        table.add_row(
+            f"{paint_status(r[0])}",
+            f"[bold]{r[1]}[/]",
+            f"{r[2]}",
+            f"{r[3]}",
+            f"{round(r[4] / 2**30, 2)} GB",
+            f"{round(r[5] / 2**30, 2)} GB",
+            f"{r[6]} CPU",
+            f"{round(r[7], 2)} CPU",
+            f"{round(r[8] / 2**30, 2)} GB",
+            f"{r[9]}"
+        )
+    
+    console.print(table)
 
 @click.command(
     help = "Show misc. stats"
